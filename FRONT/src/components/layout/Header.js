6f959ad207095302fe333c1c0dfbca5f80fe8003
Shravan Anbhule <work.shravan@gmail.com>
2020-10-26 11:17:36 +0530

    title changed

--- FRONT/src/components/layout/Header.js
+++ FRONT/src/components/layout/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header id="header">
       <Link to="/">
-        <h1 className="title">MERN auth template</h1>
+        <h1 className="title">mini.mal</h1>
       </Link>
       <AuthOptions />
     </header>
